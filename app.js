587cf5cdd1879e7666f8bc5caccb3a9381318b16
Jeff Simonitto <jsimonitto@gmail.com>
2024-07-24 23:23:55 -0400

    ci script test commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const http = require("node:http");
 const PORT = 3000;
 
 http.createServer(function (req, res) {
-    res.write("On the way to being a full stack engineer!");
+    res.write("Testing my brand new script!!!");
     res.end();
 }).listen(PORT, () => {
     console.log(`Server started on port ${PORT}.`);
